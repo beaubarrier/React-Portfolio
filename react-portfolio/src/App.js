06c0c5b2d495169a1bc76edefddca39d735ac318
Beau Barrier <beaubarrier@gmail.com>
2021-08-22 14:34:08 -0700

    Updated raw html, added component files

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save testt to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div id="logo">
+          <p id="logoText">beauBarrier</p>
+          <i id="pHolderIcon" class="fas fa-2x fa-user-astronaut"></i>
+        </div>
+        <div id="navLinks">
+          <a href="#"><i id="portraitIcon" class="fas fa-2x  fa-portrait"></i></a>
+          <a href="#"><i id="myStoryIcon" class="fas fa-2x fa-book"></i></a>
+          <a href="#"><i id="githubIcon" class="fab fa-2x  fa-github"></i></a>
+          <a href="#"><i id="emailIcon" class="fas fa-2x fa-at"></i></a>
+        </div>
+      </nav>
+
+      <div className="mainWindow">
+        <div className="main">
+
+        </div>
+      </div>
     </div>
   );
 }
