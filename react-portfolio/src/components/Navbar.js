59398b0dc9d2248c9b6d357693e8bb96fc85a77f
Beau Barrier <beaubarrier@gmail.com>
2021-08-23 13:26:23 -0700

    Working on styling

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function Navbar({ currentPage, handlePageChange }) {
 
     const [textColor, setTextColor] = useState('#FA00FF')
-
+    const [text, setText] = useState('beauBarrier')
     const randoColor = () => {
         var colors = ['#FA00FF', '#FFF500', '#05FF00', '#FF9900', '#00C2FF', '#FF0000', '#FF8CE6'];
         return colors[Math.floor((Math.random() * colors.length))]
@@ -12,7 +12,21 @@ function Navbar({ currentPage, handlePageChange }) {
     const handleChangeTextColor = (e) => {
         setTextColor(randoColor)
     }
-
+    const handleTextChangePortfolio = (e) => {
+        setText('-Portfolio-')
+    }
+    const handleTextChangeMyStory = (e) => {
+        setText(' --MyStory--')
+    }
+    const handleTextChangeGithub = (e) => {
+        setText('--Github---')
+    }
+    const handleTextChangeEmail = (e) => {
+        setText('--EmailMe--')
+    }
+    const handleTextBack = (e) => {
+        setText('beauBarrier')
+    }
     return (
         <nav className="navbar">
 
@@ -23,7 +37,7 @@ function Navbar({ currentPage, handlePageChange }) {
                     onClick={ () => { handlePageChange('default') } }
                     onMouseOver={ () => { handleChangeTextColor() } }>
 
-                    <p id="logoText" style={ { color: textColor } }>beauBarrier</p></a>
+                    <p id="logoText" style={ { color: textColor } }>{ text }</p></a>
 
                 <i id="pHolderIcon" className="fas fa-2x fa-user-astronaut"></i>
 
@@ -31,17 +45,20 @@ function Navbar({ currentPage, handlePageChange }) {
 
             <div id="navLinks">
 
-                <a href="#portfolio" onClick={ () => { handlePageChange('portfolio') } } ><i id="portraitIcon" className="fas fa-2x  fa-portrait" onMouseOver={ () => { handleChangeTextColor() } }></i></a>
+                <a href="#portfolio" onClick={ () => { handlePageChange('portfolio') } } ><i id="portraitIcon" className="fas fa-2x  fa-portrait" onMouseOver={ () => { handleChangeTextColor(); handleTextChangePortfolio(); } }
+                    onMouseOut={ () => { handleTextBack() } }
+                ></i></a>
 
-                <a href="#myStory" onClick={ () => { handlePageChange('myStory') } }  > <i id="myStoryIcon" className="fas fa-2x fa-book" onMouseOver={ () => { handleChangeTextColor() } }></i></a>
+                <a href="#myStory" onClick={ () => { handlePageChange('myStory') } }  > <i id="myStoryIcon" className="fas fa-2x fa-book" onMouseOver={ () => { handleChangeTextColor(); handleTextChangeMyStory() } }></i></a>
 
-                <a href="https://www.github.com/beaubarrier"><i id="githubIcon" className="fab fa-2x  fa-github" onMouseOver={ () => { handleChangeTextColor() } }></i></a>
+                <a href="https://www.github.com/beaubarrier"><i id="githubIcon" className="fab fa-2x  fa-github" onMouseOver={ () => { handleChangeTextColor(); handleTextChangeGithub() } } onMouseOut={ () => { handleTextBack() } }></i></a>
 
-                <a href="mailto:beaubarrier@gmail.com"><i id="emailIcon" className="fas fa-2x fa-at" onMouseOver={ () => { handleChangeTextColor() } }></i></a>
+                <a href="mailto:beaubarrier@gmail.com"><i id="emailIcon" className="fas fa-2x fa-at" onMouseOver={ () => { handleChangeTextColor(); handleTextChangeEmail() } } onMouseOut={ () => { handleTextBack() } }></i></a>
 
             </div>
 
         </nav >
+
     )
 }
 
